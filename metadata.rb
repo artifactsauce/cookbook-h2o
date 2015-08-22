c581e9ede980894d7b75594ff8b6c445927acf72
Kenji Akiyama <artifactsauce@gmail.com>
2015-08-22 15:30:34 +0900

    change the name and email.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'h2o'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Kenji Akiyama'
+maintainer_email 'artifactsauce@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures h2o'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: h2o
 # Recipe:: default
 #
-# Copyright 2015, YOUR_COMPANY_NAME
+# Copyright 2015, Kenji Akiyama
 #
 # All rights reserved - Do Not Redistribute
 #
